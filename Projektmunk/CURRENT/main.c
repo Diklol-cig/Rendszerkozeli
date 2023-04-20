fc92d71485bf9103feb28be3383f4dd29e4d950d
Diklol-cig <63556954+Diklol-cig@users.noreply.github.com>
2023-04-20 18:31:18 +0200

    8.Feladat kész

--- Projektmunk/CURRENT/main.c
+++ Projektmunk/CURRENT/main.c
@@ -1,4 +1,4 @@
-#include "myheader.h"
+#include "RKPheader.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -12,6 +12,7 @@
 #include<unistd.h>
 #include<netinet/in.h>
 #include<arpa/inet.h>
+#include<omp.h>
 
 
 int main(int argc, char* argv[]) {
